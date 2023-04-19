1e4a2390b5d7dbfa66b37194b02217c6ce2ea801
AriaHemmati <prefix.suffix.aria@gmail.com>
2023-04-19 17:00:04 +0330

    just checking

--- StrongholdCrusader/src/main/java/org/example/Main.java
+++ StrongholdCrusader/src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
 public class Main {
-    public static void main( String[] args ){
-        System.out.println( "Hello world!" );
+    public static void main( String[] args )
+    {
+        System.out.println( "Ariya is still testing" );
+        int n = 10;
+        System.out.println(n);
     }
 }
