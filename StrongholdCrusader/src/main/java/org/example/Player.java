531f6db55ac77219830c16f6aa39c2b7764c6bad
Danial-Hosseintabar <danial.ht10@gmail.com>
2023-04-19 18:15:19 +0330

    Player class added

---
+++ StrongholdCrusader/src/main/java/org/example/Player.java
@@ -0,0 +1,68 @@
+package org.example;
+
+public class Player {
+
+    private int fearRate ;
+    private int taxRate ;
+    private int foodRate ;
+    private int religionRate ;
+    // private Account account ;
+    private int pitchCapacity ;
+    private int meatCapacity ;
+    private int breadCapacity ;
+    private int cheeseCapacity ;
+    private int hop ;
+    private int cheese ;
+    private int bread ;
+    private int meat ;
+    private int pitch ;
+    private int apple ;
+    private int wineUsage ;
+    private int gold ;
+    private int stone ;
+    private int wood ;
+    private int iron ;
+    private int population ;
+    private int populationCapacity ;
+    // private ArrayList< Unit > units ;
+    // private ArrayList< Building > buildings ;
+    private int popularity ;
+
+    public void updatePopularity(){
+        // update popularity based on different parameters
+    }
+
+    public void updatePopulationCapacity(){
+        // updating population capacity based on different parameters
+    }
+
+    public void setTaxRate( int taxRate ){
+        this.taxRate = taxRate ;
+    }
+
+    public int getTaxRate(){
+        return this.taxRate ;
+    }
+
+    public int getReligionRate(){
+        return this.religionRate ;
+    }
+
+    // public Account getAccount(){}
+
+    public String getName(){
+        return null ;
+        // return name
+        // return this.account.getName() ;
+    }
+
+    public String getSlogan(){
+        return null ;
+        // return slogan
+    }
+
+    public void updatePopulation(){
+        // update population
+    }
+
+}
