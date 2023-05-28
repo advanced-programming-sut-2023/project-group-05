9f535176d85f9325e6c2cc9346a98fe0c212cfa5
Danial-Hosseintabar <danial.ht10@gmail.com>
2023-05-29 01:56:45 +0330

    sign up menu

--- StrongholdCrusader/src/main/java/org/example/view/SignupMenu.java
+++ StrongholdCrusader/src/main/java/org/example/view/SignupMenu.java
@@ -2,12 +2,58 @@ package org.example.view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import org.example.controller.SignupLoginMenuController;
 
 public class SignupMenu extends Application {
+    public TextField usernameTextField;
+    public PasswordField passwordField ;
+    public TextField emailTextField;
+    public TextField nicknameTextField;
+    public TextField sloganTextField;
+
     @Override
     public void start(Stage stage) throws Exception {
-        //Pane pane = FXMLLoader.load(StartMenu.class.getResource("/fxml/SignupMenu.fxml").toExternalForm());
+        BorderPane borderPane = FXMLLoader.load( getClass().getResource( "/fxml/SignupMenu.fxml" ) ) ;
+        stage.setScene( new Scene(borderPane) ) ;
+        stage.show() ;
+    }
+
+    public void submit( MouseEvent mouseEvent ){
+
+        boolean validUsername = SignupLoginMenuController.validUserName( usernameTextField.getText() ) ;
+        boolean validEmail = SignupLoginMenuController.validEmail( emailTextField.getText() ) ;
+        boolean validPassword = SignupLoginMenuController.validPassword( passwordField.getText() ) ;
+        boolean validNickname = SignupLoginMenuController.validNickname( nicknameTextField.getText() ) ;
+        String slogan = sloganTextField.getText() ;
+
+        if( !validUsername ){
+            (new Alert( Alert.AlertType.ERROR , "INVALID USERNAME" )).showAndWait() ;
+            return ;
+        }
+        if( !validEmail ){
+            (new Alert( Alert.AlertType.ERROR , "INVALID EMAIL" )).showAndWait() ;
+            return ;
+        }
+        if( !validPassword ){
+            (new Alert( Alert.AlertType.ERROR , "INVALID PASSWORD" )).showAndWait() ;
+            return ;
+        }
+        if( !validNickname ){
+            (new Alert( Alert.AlertType.ERROR , "INVALID NICKNAME" )).showAndWait() ;
+            return ;
+        }
+        if( slogan.equals("") ){
+            (new Alert( Alert.AlertType.ERROR , "INVALID SLOGAN" )).showAndWait() ;
+            return ;
+        }
+
     }
 }
