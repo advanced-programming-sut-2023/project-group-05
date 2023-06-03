39a16965d4efa714be3d7fbcbcfdddb4f3288a3e
Mohammad Amin Kinai <moaminkiani1@gmail.com>
2023-06-03 19:59:15 +0330

    Menu Added.

--- StrongholdCrusader/src/main/java/org/example/controller/GameGraphicalController.java
+++ StrongholdCrusader/src/main/java/org/example/controller/GameGraphicalController.java
@@ -1,6 +1,7 @@
 package org.example.controller;
 
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -8,6 +9,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
@@ -17,85 +19,178 @@ import org.example.model.BuildingImages;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class GameGraphicalController {
 
-    private static Stage stage ;
-    private static Pane pane ;
-    private static Polygon[][] map ;
-    private static int width ;
-    private static int height ;
-    private static Rectangle graphicalMenu ;
-    private static ArrayList <Rectangle> graphicalMenuButton ;
-    private static ArrayList <Rectangle> buildings ;
-    private static int SCREEN_HEIGHT ;
-    private static int SCREEN_WIDTH ;
-    private static final int TILE_HEIGHT = 20 ;
-    private static final int TILE_WIDTH = TILE_HEIGHT * 2 ;
-    private static Camera camera ;
-    public static ArrayList<Shape> reservedShapes ;
+    private static Stage stage;
+    public static ArrayList<Image> armouries = new ArrayList<>();
+    public static ArrayList<Image> foods = new ArrayList<>();
+    public static ArrayList<Image> houseAndStorage = new ArrayList<>();
+    public static ArrayList<Image> militaryBuildings = new ArrayList<>();
+    public static ArrayList<Image> goodThings = new ArrayList<>();
+    public static ArrayList<Image> badThings = new ArrayList<>();
+    public static ArrayList<Image> towerAndWalls = new ArrayList<>();
+    public static ArrayList<Image> industries = new ArrayList<>();
 
-    public static void init( Stage stage , Pane pane ){
-        GameGraphicalController.pane = pane ;
-        GameGraphicalController.stage = stage ;
-        reservedShapes = new ArrayList<>() ;
-        width = 400 ;
-        height = 400 ;
-        SCREEN_HEIGHT = 700 ;
-        SCREEN_WIDTH = 1000 ;
-        pane.requestFocus() ;
-        initMap( height , width ) ;
-        initImages() ;
-        initKeyboardControlKeys() ;
-        initGraphicalMenu() ;
+
+    private static Pane pane;
+    private static Polygon[][] map;
+    private static int width;
+    private static int height;
+    private static Rectangle graphicalMenu;
+    private static ArrayList<Rectangle> graphicalMenuButton;
+    private static ArrayList<Rectangle> buildings;
+    private static int SCREEN_HEIGHT;
+    private static int SCREEN_WIDTH;
+    private static final int TILE_HEIGHT = 20;
+    private static final int TILE_WIDTH = TILE_HEIGHT * 2;
+    private static Camera camera;
+    public static ArrayList<Shape> reservedShapes;
+
+    public static void init(Stage stage, Pane pane) {
+        GameGraphicalController.pane = pane;
+        GameGraphicalController.stage = stage;
+        reservedShapes = new ArrayList<>();
+        width = 400;
+        height = 400;
+        SCREEN_HEIGHT = 700;
+        SCREEN_WIDTH = 1000;
+        pane.requestFocus();
+        initMap(height, width);
+        initImages();
+        initKeyboardControlKeys();
+        initGraphicalMenu();
     }
 
-    private static void initImages(){
+    private static void initImages() {
         // init images
     }
 
-    private static void initMap( int height , int width ){
+    private static void initMap(int height, int width) {
         map = new Polygon[height][width];
-        camera = new Camera( map , pane );
+        camera = new Camera(map, pane);
         camera.draw();
         camera.move(0);
     }
 
-    public static void initKeyboardControlKeys(){
+    public static void initKeyboardControlKeys() {
         pane.requestFocus();
-        pane.setOnKeyPressed( new EventHandler <KeyEvent>() {
+        pane.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
-            public void handle( KeyEvent keyEvent ){
-                if( keyEvent.getCode().equals( KeyCode.DOWN ) ){
-                    camera.move( 1 );
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode().equals(KeyCode.DOWN)) {
+                    camera.move(1);
                 }
-                if( keyEvent.getCode().equals( KeyCode.UP ) ){
-                    camera.move( 0 ) ;
+                if (keyEvent.getCode().equals(KeyCode.UP)) {
+                    camera.move(0);
                 }
-                if( keyEvent.getCode().equals( KeyCode.LEFT ) ){
-                    camera.move( 3 ) ;
+                if (keyEvent.getCode().equals(KeyCode.LEFT)) {
+                    camera.move(3);
                 }
-                if( keyEvent.getCode().equals( KeyCode.RIGHT ) ){
-                    camera.move( 2 ) ;
+                if (keyEvent.getCode().equals(KeyCode.RIGHT)) {
+                    camera.move(2);
                 }
             }
-        } ) ;
+        });
     }
 
-    private static int selectedButton = 0 ;
+    private static int selectedButton = 0;
+    public static ArrayList<Rectangle> current;
+    public static ArrayList<Circle> buttons = new ArrayList<>();
+    public static HashMap<Circle, ArrayList<Rectangle>> buttonMap = new HashMap<>();
+    public static ArrayList<ArrayList<Rectangle>> menuMap = new ArrayList<>();
 
-    public static void initGraphicalMenu(){
-        graphicalMenu = new Rectangle(0,SCREEN_HEIGHT*0.8,SCREEN_WIDTH,SCREEN_HEIGHT*0.2);
+    public static void initGraphicalMenu() {
+        graphicalMenu = new Rectangle(0, SCREEN_HEIGHT * 0.7, SCREEN_WIDTH, SCREEN_HEIGHT * 0.3);
+        graphicalMenu.setFill(new ImagePattern(BuildingImages.MENU.getImage()));
         reservedShapes.add(graphicalMenu);
         pane.getChildren().add(graphicalMenu);
-        graphicalMenu.setFill(new ImagePattern(BuildingImages.MENU.getImage()));
+        ArrayList<Rectangle> armoury = new ArrayList<>();
+        initArray(armoury, armouries, 40, 40);
+        ArrayList<Rectangle> food = new ArrayList<>();
+        initArray(food, foods, 30, 30);
+        ArrayList<Rectangle> houseAndStorages = new ArrayList<>();
+        initArray(houseAndStorages, houseAndStorage, 30, 30);
+        ArrayList<Rectangle> industry = new ArrayList<>();
+        initArray(industry, industries, 40, 40);
+        ArrayList<Rectangle> militaryBuilding = new ArrayList<>();
+        initArray(militaryBuilding, militaryBuildings, 40, 40);
+        ArrayList<Rectangle> goodThing = new ArrayList<>();
+        initArray(goodThing, goodThings, 40, 40);
+        ArrayList<Rectangle> badThing = new ArrayList<>();
+        initArray(badThing, badThings, 40, 40);
+        ArrayList<Rectangle> towerAndWall = new ArrayList<>();
+        initArray(towerAndWall, towerAndWalls, 20, 40);
+        for (Rectangle rectangle : houseAndStorages)
+            pane.getChildren().add(rectangle);
+        current = houseAndStorages;
+        setMenuButtons(pane);
+    }
+
+    public static void setMenuButtons(Pane pane) {
+        Circle house = new Circle(SCREEN_HEIGHT * 0.95, SCREEN_HEIGHT * 0.95 - 90, 15);
+        house.setFill(Color.BLUE);
+        buttonMap.put(house,menuMap.get(2));
+        Circle food = new Circle(SCREEN_HEIGHT * 0.95-30, SCREEN_HEIGHT * 0.95 - 90, 15);
+        food.setFill(Color.RED);
+        buttonMap.put(food,menuMap.get(1));
+        Circle armoury = new Circle(SCREEN_HEIGHT * 0.95-60, SCREEN_HEIGHT * 0.95 - 90, 15);
+        armoury.setFill(Color.YELLOW);
+        buttonMap.put(armoury,menuMap.get(0));
+        Circle military = new Circle(SCREEN_HEIGHT * 0.95-90, SCREEN_HEIGHT * 0.95 - 90, 15);
+        buttonMap.put(military,menuMap.get(4));
+        Circle good = new Circle(SCREEN_HEIGHT * 0.95-120, SCREEN_HEIGHT * 0.95- 90, 15);
+        buttonMap.put(good,menuMap.get(5));
+        Circle bad = new Circle(SCREEN_HEIGHT * 0.95-150, SCREEN_HEIGHT * 0.95- 90, 15);
+        buttonMap.put(bad,menuMap.get(6));
+        Circle towers = new Circle(SCREEN_HEIGHT * 0.95-180, SCREEN_HEIGHT * 0.95- 90, 15);
+        buttonMap.put(towers,menuMap.get(7));
+        Circle industry = new Circle(SCREEN_HEIGHT * 0.95-210, SCREEN_HEIGHT * 0.95 - 90, 15);
+        buttonMap.put(industry,menuMap.get(3));
+        pane.getChildren().addAll(house, food, armoury, military, good, bad, towers, industry);
+        buttons.add(house);
+        buttons.add(food);
+        buttons.add(armoury);
+        buttons.add(military);
+        buttons.add(good);
+        buttons.add(bad);
+        buttons.add(towers);
+        buttons.add(industry);
+        for (Circle circle : buttons) {
+            circle.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent mouseEvent) {
+                    alterMenu(current,buttonMap.get(circle),pane);
+                    current = buttonMap.get(circle);
+                }
+            });
+        }
 
     }
 
-    public static ImagePattern loadImagePattern( URL url ){
-        ImagePattern imagePattern ;
-        imagePattern = new ImagePattern( new Image( url.toExternalForm() ) ) ;
-        return imagePattern ;
+    public static void alterMenu(ArrayList<Rectangle> first, ArrayList<Rectangle> second, Pane pane) {
+        for (Rectangle rectangle : first) pane.getChildren().remove(rectangle);
+        for (Rectangle rectangle : second) pane.getChildren().add(rectangle);
+    }
+
+    public static void initArray(ArrayList<Rectangle> array, ArrayList<Image> images, int height, int width) {
+        int size = images.size();
+        for (int i = 0; i < size; ++i) {
+            Rectangle rectangle = new Rectangle(150 + SCREEN_WIDTH * ((double) i / size) * 0.5, SCREEN_HEIGHT * 0.85, height * 2, width * 2.5);
+            rectangle.setFill(new ImagePattern(images.get(i)));
+            //TODO : SETON CLICK ON RECTANGLE
+            array.add(rectangle);
+        }
+        menuMap.add(array);
+        reservedShapes.addAll(array);
+    }
+
+    public static ImagePattern loadImagePattern(URL url) {
+        ImagePattern imagePattern;
+        imagePattern = new ImagePattern(new Image(url.toExternalForm()));
+        return imagePattern;
     }
 
 }
--- StrongholdCrusader/src/main/java/org/example/model/BuildingImages.java
+++ StrongholdCrusader/src/main/java/org/example/model/BuildingImages.java
@@ -2,6 +2,7 @@ package org.example.model;
 
 import javafx.scene.image.Image;
 import org.example.Main;
+import org.example.controller.GameGraphicalController;
 import org.example.view.MainMenu;
 
 import java.util.ArrayList;
@@ -10,76 +11,88 @@ import java.util.Arrays;
 public enum BuildingImages {
 //    SMALL_STONE_WALL(),
 //    BIG_STONE_WALL(),
-    SMALL_STONE_GATEHOUSE("towersAndWalls/smallGateHouse.png"),
-    BIG_STONE_GATEHOUSE("towersAndWalls/bigGateHouse.png"),
-    DRAW_BRIDGE("towersAndWalls/drawBridge.png"),
-    LOOKOUT_TOWER("towersAndWalls/lookoutTower.png"),
-    PERIMETER_TOWER("towersAndWalls/perimeterTower.png"),
+    SMALL_STONE_GATEHOUSE("towersAndWalls/smallGateHouse.png",7),
+    BIG_STONE_GATEHOUSE("towersAndWalls/bigGateHouse.png",7),
+    DRAW_BRIDGE("towersAndWalls/drawBridge.png",7),
+    LOOKOUT_TOWER("towersAndWalls/lookoutTower.png",7),
+    PERIMETER_TOWER("towersAndWalls/perimeterTower.png",7),
     //DEFENCE_TURRET(),
-    SQUARE_TOWER("towersAndWalls/squareTower.png"),
-    ROUND_TOWER("towersAndWalls/roundTower.png"),
-    ARMOURY("militaryBuildings/armoury.png"),
-    BARRACKS("militaryBuildings/barracks.png"),
-    MERCENARY_POST("militaryBuildings/mercenaryPost.png"),
-    ENGINEER_GUILD("militaryBuildings/engineerGuild.png"),
+    SQUARE_TOWER("towersAndWalls/squareTower.png",7),
+    ROUND_TOWER("towersAndWalls/roundTower.png",7),
+    ARMOURY("militaryBuildings/armoury.png",4),
+    BARRACKS("militaryBuildings/barracks.png",4),
+    MERCENARY_POST("militaryBuildings/mercenaryPost.png",4),
+    ENGINEER_GUILD("militaryBuildings/engineerGuild.png",4),
 //    KILLING_PIT(),
-    OIL_SMELTER("towersAndWalls/traps/oilSmelter.png"),
+    OIL_SMELTER("towersAndWalls/traps/oilSmelter.png",7),
 //    PITCH_DITCH(),
-    CAGED_WAR_DOGS("towersAndWalls/traps/dogTrap.png"),
-    SIEGE_TENT("militaryBuildings/siegeTent.png"),
-    STABLE("militaryBuildings/stable.png"),
-    TUNNELER_GUILD("militaryBuildings/tunnelerGuild.png"),
-    APPLE_ORCHARD("foods/appleFarm.png"),
-    DIARY_FARMER("foods/cheeseFarm.png"),
-    HOPS_FARMER("foods/hopsFarm.png"),
-    HUNTER_POST("foods/hunters.png"),
-    WHEAT_FARM("foods/wheatFarm.png"),
-    BAKERY("foods/bakery.png"),
-    BREWER("foods/brewer.png"),
-    GRANARY("houseAndStorage/granary.png"),
-    INN("foods/inn.png"),
-    MILL("foods/windMill.png"),
-    IRON_MINE("industries/ironMine.png"),
-    MARKET("houseAndStorage/market.png"),
-    OX_TETHER("industries/oxTether.png"),
-    PITCH_RIG("industries/pitch.png"),
-    QUARRY("industries/quarry.png"),
-    STOCKPILE("houseAndStorage/stockpile.png"),
-    WOODCUTTER("industries/woodCutter.png"),
-    APOTHECARY("popularityBuildings/goodThings/Apothecary.png"),
-    HOVEL("houseAndStorage/house.png"),
-    CHAPEL("popularityBuildings/goodThings/chapel.png"),
-    CHURCH("popularityBuildings/goodThings/church.png"),
-    CATHEDRAL("popularityBuildings/goodThings/cathedral.png"),
-    WELL("houseAndStorage/waterWell.png"),
-    WATER_POT("houseAndStorage/waterPot.png"),
-    GOOD_THINGS_1("popularityBuildings/goodThings/shrine.png"),
-    GOOD_THINGS_2("popularityBuildings/goodThings/status.png"),
-    GOOD_THINGS_3("popularityBuildings/goodThings/maypole.png"),
-    GOOD_THINGS_4("popularityBuildings/goodThings/garden.png"),
-    BAD_THINGS_1("popularityBuildings/badThings/bad1.png"),
-    BAD_THINGS_2("popularityBuildings/badThings/bad2.gif"),
-    BAD_THINGS_3("popularityBuildings/badThings/bad3.gif"),
-    BAD_THINGS_4("popularityBuildings/badThings/bad4.gif"),
-    BAD_THINGS_5("popularityBuildings/badThings/bad5.gif"),
-    BAD_THINGS_6("popularityBuildings/badThings/bad6.gif"),
-    BAD_THINGS_7("popularityBuildings/badThings/bad7.gif"),
-    BAD_THINGS_8("popularityBuildings/badThings/bad8.gif"),
-    ARMOURER("armouries/armourer.png"),
-    BLACKSMITH("armouries/blackSmith.png"),
-    FLETCHER("armouries/fletcher.png"),
-    POLE_TURNER("armouries/poleTurner.png"),
-    TANNER("armouries/tanner.png"),
-    SIGNPOST("houseAndStorage/signPost.png"),
-    MENU("menu.png");
+    CAGED_WAR_DOGS("towersAndWalls/traps/dogTrap.png",7),
+    SIEGE_TENT("militaryBuildings/siegeTent.png",4),
+    STABLE("militaryBuildings/stable.png",4),
+    TUNNELER_GUILD("militaryBuildings/tunnelerGuild.png",4),
+    APPLE_ORCHARD("foods/appleFarm.png",2),
+    DIARY_FARMER("foods/cheeseFarm.png",2),
+    HOPS_FARMER("foods/hopsFarm.png",2),
+    HUNTER_POST("foods/hunters.png",2),
+    WHEAT_FARM("foods/wheatFarm.png",2),
+    BAKERY("foods/bakery.png",2),
+    BREWER("foods/brewer.png",2),
+    GRANARY("houseAndStorage/granary.png",3),
+    INN("foods/inn.png",2),
+    MILL("foods/windMill.png",2),
+    IRON_MINE("industries/ironMine.png",8),
+    MARKET("houseAndStorage/market.png",3),
+    OX_TETHER("industries/oxTether.png",8),
+    PITCH_RIG("industries/pitch.png",8),
+    QUARRY("industries/quarry.png",8),
+    STOCKPILE("houseAndStorage/stockpile.png",3),
+    WOODCUTTER("industries/woodCutter.png",8),
+    APOTHECARY("popularityBuildings/goodThings/Apothecary.png",5),
+    HOVEL("houseAndStorage/house.png",3),
+    CHAPEL("popularityBuildings/goodThings/chapel.png",5),
+    CHURCH("popularityBuildings/goodThings/church.png",5),
+    CATHEDRAL("popularityBuildings/goodThings/cathedral.png",5),
+    WELL("houseAndStorage/waterWell.png",3),
+    WATER_POT("houseAndStorage/waterPot.png",3),
+    GOOD_THINGS_1("popularityBuildings/goodThings/shrine.png",5),
+    GOOD_THINGS_2("popularityBuildings/goodThings/status.png",5),
+    GOOD_THINGS_3("popularityBuildings/goodThings/maypole.png",5),
+    GOOD_THINGS_4("popularityBuildings/goodThings/garden.png",5),
+    BAD_THINGS_1("popularityBuildings/badThings/bad1.png",6),
+    BAD_THINGS_2("popularityBuildings/badThings/bad2.gif",6),
+    BAD_THINGS_3("popularityBuildings/badThings/bad3.gif",6),
+    BAD_THINGS_4("popularityBuildings/badThings/bad4.gif",6),
+    BAD_THINGS_5("popularityBuildings/badThings/bad5.gif",6),
+    BAD_THINGS_6("popularityBuildings/badThings/bad6.gif",6),
+    BAD_THINGS_7("popularityBuildings/badThings/bad7.gif",6),
+    BAD_THINGS_8("popularityBuildings/badThings/bad8.gif",6),
+    ARMOURER("armouries/armourer.png",1),
+    BLACKSMITH("armouries/blackSmith.png",1),
+    FLETCHER("armouries/fletcher.png",1),
+    POLE_TURNER("armouries/poleTurner.png",1),
+    TANNER("armouries/tanner.png",1),
+    SIGNPOST("houseAndStorage/signPost.png",3),
+    MENU("menu.png",0);
 //    TUNNEL_ENTRANCE(),
 //    STAIR();
 
     private Image image ;
 
-    BuildingImages( String buildingImageName ){
-        System.out.println(buildingImageName);
+    private int category;
+    BuildingImages( String buildingImageName,int category ){
         this.image = new Image(MainMenu.class.getResource( "/images/buildings/buildingMenu/"+buildingImageName).toExternalForm() ) ;
+        switch (category) {
+            case 1 -> GameGraphicalController.armouries.add(image);
+            case 2 -> GameGraphicalController.foods.add(image);
+            case 3 -> GameGraphicalController.houseAndStorage.add(image);
+            case 4 -> GameGraphicalController.militaryBuildings.add(image);
+            case 5 -> GameGraphicalController.goodThings.add(image);
+            case 6 -> GameGraphicalController.badThings.add(image);
+            case 7 -> GameGraphicalController.towerAndWalls.add(image);
+            case 8 -> GameGraphicalController.industries.add(image);
+            default -> {
+            }
+        }
     }
 
     public Image getImage(){
