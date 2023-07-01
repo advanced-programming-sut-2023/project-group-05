eeb641108e59524ae4bdb40f097a564032746327
AriaHemmati <prefix.suffix.aria@gmail.com>
2023-07-01 13:07:34 +0330

    Graphics Phase3 is done

--- client/StrongholdCrusader/src/main/java/org/example/view/ChatMenu.java
+++ client/StrongholdCrusader/src/main/java/org/example/view/ChatMenu.java
@@ -15,13 +15,28 @@ import javafx.stage.Stage;
 import org.example.controller.URLFinder;
 
 public class ChatMenu extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
-        stage.setTitle("Chat Room");
-        TabPane tabPane = FXMLLoader.load(URLFinder.run("/fxml/ChatMenu.fxml"));
+    public static Tab publicChat;
+    public static Tab privateChat;
+    public static void updatePublicChat()
+    {
+        ScrollPane publicChats = new ScrollPane();
+        VBox pChats = new VBox();
+        ///TODO: fill this public and private Chats field with JSON file from server
+        for(Node label : pChats.getChildren())
+        {
+            label.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent mouseEvent) {
+                    System.out.println("Enter the public chat with ");
 
-        Tab publicChat = tabPane.getTabs().get(0);
-        Tab privateChat = tabPane.getTabs().get(1);
+                }
+            });
+        }
+        publicChats.setContent(pChats);
+    }
+
+    public static void updatePrivateChat()
+    {
         ScrollPane privateChats = new ScrollPane();
         VBox pChats = new VBox();
         ///TODO: fill this public and private Chats field with JSON file from server
@@ -30,12 +45,24 @@ public class ChatMenu extends Application {
             label.setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent mouseEvent) {
-                    System.out.println("Enter the chat with ");
+                    System.out.println("Enter the private chat with ");
 
                 }
             });
         }
         privateChats.setContent(pChats);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Chat Room");
+        TabPane tabPane = FXMLLoader.load(URLFinder.run("/fxml/ChatMenu.fxml"));
+
+        publicChat = tabPane.getTabs().get(0);
+        privateChat = tabPane.getTabs().get(1);
+
+        updatePrivateChat();
+        updatePublicChat();
 
         stage.setScene(new Scene(tabPane));
         stage.show();
